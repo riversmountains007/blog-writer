1ec3e68cc6a7d11b5f2fa96e653d0dd37b8ec598
riversmountains007 <riversmountains007@gmail.com>
2025-04-23 19:59:30 +0530

    ./header to ./Header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Header from './header'
-import Footer from './footer'
+import Header from './Header'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
-
 
 function Layout() {
   return (
